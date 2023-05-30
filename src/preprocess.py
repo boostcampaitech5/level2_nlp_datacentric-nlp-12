6d3f52ede0fe64b1b7c96e0140024eb2510da332
iamzieun <iamhappyhill@yonsei.ac.kr>
2023-05-30 12:41:22 +0000

    feat: add preprocessing script

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,5 +1,13 @@
 import pandas as pd
+import re
 
 
 def preprocess_df(df: pd.DataFrame) -> pd.DataFrame:
+    
+    # 한자 제거
+    #df['text'] = [re.sub('[一-龥]', '', text) for text in df['text']]
+    
+    # 특수문자 제거
+    df['text'] = [re.sub(r'[^\w\s\u4E00-\u9FFF]', '', text) for text in df['text']]
+
     return df
