8a7fc422c6346e240433b421177a624c82498000
iamzieun <iamhappyhill@yonsei.ac.kr>
2023-05-31 12:54:24 +0000

    feat: specify added settings in config file and add docstring in preprocess.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -5,7 +5,19 @@ import hanja
 
 
 def preprocess_df(df: pd.DataFrame, hanja: bool = False, special: bool = False, jonghab: bool = False) -> pd.DataFrame:
-    
+    """
+    주어진 데이터프레임을 전처리합니다.
+
+    Parameters:
+        df (pd.DataFrame): 전처리할 데이터프레임.
+        hanja (bool, optional): 한자를 한글로 대체할지 여부를 나타내는 인자. 기본값은 False.
+        special (bool, optional): 특수문자 제거 여부를 나타내는 인자. 기본값은 False.
+        jonghab (bool, optional): 문장 마지막 어절의 '종합 ~' 패턴 제거 여부를 나타내는 인자. 기본값은 False.
+
+    Returns:
+        pd.DataFrame: 전처리가 적용된 데이터프레임.
+
+    """
     # 한자 한국어로 대체
     if hanja == True:
         df['text'] = [hanja.translate(text, 'substitution') for text in df['text']]
