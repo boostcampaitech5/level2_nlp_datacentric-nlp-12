63dc3e934d0c54ad09f496289f8f3b4ee639ff01
iamzieun <iamhappyhill@yonsei.ac.kr>
2023-05-31 08:58:47 +0000

    feat: add preprocessing script and modify related part in run.py

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,13 +1,22 @@
 import pandas as pd
 import re
 
+import hanja
 
-def preprocess_df(df: pd.DataFrame) -> pd.DataFrame:
+
+def preprocess_df(df: pd.DataFrame, hanja: bool = False, special: bool = False, jonghab: bool = False) -> pd.DataFrame:
     
-    # 한자 제거
-    #df['text'] = [re.sub('[一-龥]', '', text) for text in df['text']]
-    
+    # 한자 한국어로 대체
+    if hanja == True:
+        df['text'] = [hanja.translate(text, 'substitution') for text in df['text']]
+
     # 특수문자 제거
-    df['text'] = [re.sub(r'[^\w\s\u4E00-\u9FFF]', '', text) for text in df['text']]
+    if special == True:
+        df['text'] = [re.sub(r'[^\w\s\u4E00-\u9FFF]', ' ', text) for text in df['text']]
+
+    # '종합 ~' 제거
+    if jonghab == True:
+        pattern = re.compile(r'(.*)\s종합$')
+        df['text'] = df['text'].apply(lambda x: re.sub(pattern, r'\1', x))
 
     return df
